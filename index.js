29a1a611965a7893e4d8576fb90c3cfcc1061b6f
Eric <ericvondee5@gmail.com>
2023-04-20 03:11:43 +0100

    url shortener setup

---
+++ index.js
@@ -0,0 +1,41 @@
+import createError from "http-errors";
+import cors from "cors";
+import compression from "compression";
+import express from "express";
+import { PORT } from "./config/index.js";
+import Router from "./src/url-shorten.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(compression());
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome to Asgaard",
+  });
+});
+
+app.use("/", Router);
+
+//Catch 404 and forard to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    status: "ERROR",
+    message: err.message,
+    payload: { ...err },
+  });
+});
+
+app.listen(PORT, () => console.log(`URL Shortener is live on port http://0.0.0.0.:${PORT}`));
---
+++ src/url-shorten.js
@@ -0,0 +1,74 @@
+import express from "express";
+import { BASE_URL } from "../config/index.js";
+import db from "./db/index.js";
+import { generateUUID, numToBase62 } from "./utils/id-generator.js";
+import redis from "./utils/redisService.js";
+const router = express.Router();
+
+router.post("/shorten", async (req, res, next) => {
+  try {
+    const { longURL } = req.body;
+    const findUrl = await db("users").where({ longURL: longURL }).first();
+    if (findUrl) {
+      return res
+        .status(200)
+        .json({
+          statusCode: 200,
+          message: "fetched",
+          data: { shortURL: `${BASE_URL}/${findUrl.shortURL}`, longURL: findUrl.longURL },
+        });
+    }
+    const uuid = generateUUID();
+    const shortURL = numToBase62(uuid);
+    await db("users").insert({
+      shortURL: shortURL,
+      longURL: longURL,
+      uuid: uuid,
+    });
+    const params = {
+      shortURL: `${BASE_URL}/${shortURL}`,
+      longURL: longURL,
+    };
+    await redis.hmset(params.shortURL, params);
+    await redis.expire(params.shortURL, 259200);
+    res.status(200).json({ statusCode: 200, message: "created", data: params });
+  } catch (e) {
+    console.log("1", e);
+    return res.status(500).json({
+      statusCode: 500,
+      messsage: e.messsage,
+    });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let url;
+    const { id } = req.params;
+    const exists = await redis.exists(id);
+    if (exists) {
+      url = await redis.hgetall(id);
+      res.redirect(301, url.longURL);
+    }
+    if (!exists) {
+      const findUrl = await db("users").where({ shortURL: id }).first();
+      if (findUrl) {
+        await redis.hmset(findUrl.shortURL, {
+          shortURL: findUrl.shortURL,
+          longURL: findUrl.longURL,
+        });
+        await redis.expire(findUrl.shortURL, 259200);
+        res.redirect(301, findUrl.longURL);
+      } else {
+        res.status(404).json({ statusCode: 404, message: "URL not found" });
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      statusCode: 500,
+      messsage: e.messsage,
+    });
+  }
+});
+export default router;
