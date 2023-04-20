d4b7c606a9dd3eea362811e22f35d8ca2b02a593
Eric <ericvondee5@gmail.com>
2023-04-20 03:11:20 +0100

    config

---
+++ config/index.js
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const PORT = process.env.PORT || "";
+export const REDIS_HOST = process.env.REDIS_HOST || "";
+export const REDIS_PORT = process.env.REDIS_PORT || "";
+export const BASE_URL = process.env.BASE_URL || "";
+export const DB_HOST = process.env.DB_HOST || "";
+export const DB_USER = process.env.DB_USER || "";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const DB_NAME = process.env.DB_NAME || "";
