edfb2582dd17f87cb084e9f084ae59e17a97a036
Eric <ericvondee5@gmail.com>
2023-05-04 13:48:23 +0100

    cors setup

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ import { NODE_ENV, PORT } from "./config/index.js";
 import Router from "./src/url-shorten.js";
 import helmet from "helmet";
 import { limiter } from "./middleware/ratelimiter.js";
+import * as Sentry from "@sentry/node";
 const app = express();
+
+const corsConfig = {
+  origin:
+    NODE_ENV === "production"
+      ? ["http://localhost:5173/", "http://localhost:5173", "https://transcript-chain.vercel.app/"]
+      : ["http://localhost:5173"],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 app.use(limiter);
 app.disable("x-powered-by");
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(compression());
@@ -28,6 +38,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/", Router);
+app.use(Sentry.Handlers.errorHandler());
 
 //Catch 404 and forard to error handler
 app.use((req, res, next) => {
